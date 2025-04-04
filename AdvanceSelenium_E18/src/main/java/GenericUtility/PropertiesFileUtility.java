b878d6ca242f9d3cf2dce66c9165c2800916dd98
ashwinis29 <ashwini.saravannavar29@gmail.com>
2025-04-04 20:11:49 +0530

    Changes in property file

--- AdvanceSelenium_E18/src/main/java/GenericUtility/PropertiesFileUtility.java
+++ AdvanceSelenium_E18/src/main/java/GenericUtility/PropertiesFileUtility.java
@@ -9,7 +9,7 @@ public class PropertiesFileUtility {
 	
 	public String readingDataFromPropFile(String Key) throws IOException
 	{
-		FileInputStream fis = new FileInputStream("C:\\Users\\hp\\eclipse-workspace\\AdvanceSelenium_E18\\src\\test\\resources\\CommonData_E18.properties");
+		FileInputStream fis = new FileInputStream("C:\\Users\\hp\\git\\E18_Batch\\AdvanceSelenium_E18\\src\\test\\resources\\CommonData_E18.properties");
 		Properties prop = new Properties();
 		prop.load(fis);
 		String data = prop.getProperty(Key);
